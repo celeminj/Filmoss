7441e534b640b386c7c736b33d24ebe35ef2c7b9
Jordi <jceleming2324@politecnics.barcelona>
2025-03-18 18:53:24 +0100

    TicketsBackendUsuarios

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,8 +6,8 @@
     <div class="text-light">
         <h1>REGÍSTRATE</h1>
         <div class="container">
-            <form>
-
+            <form action="{{ action([App\Http\Controllers\UsuarioController::class, 'register']) }}" method="POST">
+                @csrf
                 <div data-mdb-input-init class="form-outline mb-4">
                     <label for="form4Example1" class="form-label">Nombre de usuario</label>
                     <input type="text" name="nombre" class="form-control" id="form4Example1">
@@ -31,7 +31,7 @@
                 </div>
                 <div class="mb-3 form-check">
                     <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Soy mayor de 18</label>
+                    <label class="form-check-label" for="exampleCheck1">Acepto la politica de privacidad y cookies</label>
                 </div>
                 <a href="login">¿Ya tienes cuenta? Inicia sesión</a>
                 <button type="submit" class="btn btn-secondary">Registrarme</button>
---
+++ resources/views/ticket/ticket.blade.php
@@ -0,0 +1,40 @@
+@extends('index.index')
+@section('contenido')
+    <br>
+    <br>
+    <br>
+    <div class="text-light">
+        <h1>TICKET</h1>
+        <div class="container">
+            <form>
+
+                <div data-mdb-input-init class="form-outline mb-4">
+                    <label for="form4Example1" class="form-label">Nombre del titular</label>
+                    <input type="text" name="nombre" class="form-control" id="form4Example1" placeholder="Jordi C">
+
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Dirección de correo electrónico</label>
+                    <input type="email" name="correo" class="form-control" id="exampleInputEmail1"
+                        aria-describedby="emailHelp" placeholder="filmo@gmail.com">
+
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Numero de tarjeta</label>
+                    <input type="text" name="numeroTarjeta" class="form-control" id="exampleInputPassword1"
+                        placeholder="ES01 1234 5678 9012">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Fecha de caducidad</label>
+                    <input type="date" name="fechaCaducidad" class="form-control" id="exampleInputPassword1">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">CVV</label>
+                    <input class="required" id="field" type="number" max="999" min="-999" name="cvv" />
+                </div>
+
+
+                <button type="submit" class="btn btn-secondary">PAGAR</button>
+            </form>
+        </div>
+    </div>
