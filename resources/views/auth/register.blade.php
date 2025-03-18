b5395a508f0612f66f487658de0d12e1d5ca048a
Jordi Pérez <jperezf2324@politecnics.barcelona>
2025-03-18 18:50:45 +0100

    02

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,40 +1,56 @@
 @extends('index.index')
 @section('contenido')
+    <link
+        href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    @vite(['resources/css/login.css'])
+
     <br>
     <br>
     <br>
     <div class="text-light">
-        <h1>REGÍSTRATE</h1>
-        <div class="container">
-            <form>
 
-                <div data-mdb-input-init class="form-outline mb-4">
-                    <label for="form4Example1" class="form-label">Nombre de usuario</label>
-                    <input type="text" name="nombre" class="form-control" id="form4Example1">
+        <div class="containers">
+            <div class="container-1">
+                <img src="{{ asset('images/filmo-logo.png'  )}}" alt="">
+            </div>
 
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Fecha de nacimiento</label>
-                    <input type="date" name="fecha_nacimiento" class="form-control" id="exampleInputEmail1"
-                        aria-describedby="emailHelp">
+                <div class="fondo-container-2" style="background-image: url('{{ asset('images/log-in.png') }}');">
 
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Dirección de correo electrónico</label>
-                    <input type="email" name="correo" class="form-control" id="exampleInputEmail1"
-                        aria-describedby="emailHelp">
+                <div class="container-2">
+                    <div class="fondo-container-2">
+                        <div class="titulo-registro">
+                            <h1>REGISTRO</h1>
+                        </div>
+                        <form>
+                            <div data-mdb-input-init class="form-outline mb-4">
+                                <label for="form4Example1" class="form-label">Nombre de usuario</label>
+                                <input type="text" name="nombre" class="form-control" id="form4Example1">
 
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-                    <input type="password" name="contrasenya" class="form-control" id="exampleInputPassword1">
-                </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Soy mayor de 18</label>
-                </div>
-                <a href="login">¿Ya tienes cuenta? Inicia sesión</a>
-                <button type="submit" class="btn btn-secondary">Registrarme</button>
-            </form>
-        </div>
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Dirección de correo electrónico</label>
+                                <input type="email" name="correo" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp">
+
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+                                <input type="password" name="contrasenya" class="form-control" id="exampleInputPassword1">
+                            </div>
+                            <div class="mb-3 form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Soy mayor de 18</label>
+                            </div>
+                            <a href="login">¿Ya tienes cuenta? Inicia sesión</a>
+                            <button type="submit" class="btn btn-secondary">Registrarme</button>
+                        </form>
+                    </div>
+                    </div>
+
+
+            </div>
+
+
     </div>
