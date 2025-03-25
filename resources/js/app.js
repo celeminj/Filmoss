42906bc979581aca5e4a93303c56d1f6ee95310e
Jordi <jceleming2324@politecnics.barcelona>
2025-03-25 18:49:12 +0100

    CRUDdepeliculasnuevas

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,10 +5,15 @@ import {createApp} from 'vue';
 import film from './components/Film.vue';
 // import scroll from './components/Scroll.vue';
 import UsuarioUpdate from './components/usuarioUpdate.vue';
+import PeliculasCRUD from './components/peliculasCRUD.vue';
+import eventosCRUD from './components/eventosCRUD.vue';
 
 const app = createApp({});
 
 app.component('film', film);
 // app.component('scroll', scroll);
 app.component('usuario-update', UsuarioUpdate);
+app.component('peliculas-crud', PeliculasCRUD);
+app.component('eventos-crud', eventosCRUD);
+
 app.mount('#app');
