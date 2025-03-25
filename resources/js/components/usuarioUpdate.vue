9c065a35259cbcb03acba00479d77f7172d8be51
Jordi <jceleming2324@politecnics.barcelona>
2025-03-25 17:34:15 +0100

    tiketsarreglandocosasPeerez

--- resources/js/components/usuarioUpdate.vue
+++ resources/js/components/usuarioUpdate.vue
@@ -23,7 +23,7 @@
                         <td>{{ usuario.contrasenya }}</td>
                         <td>{{ usuario.rol.tipo_rol}}</td>
 
-                        <th> <button @click="editUsuario(usuario)"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                        <th> <button @click="editUsuario(usuario)" class="editarBoton"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                                     fill="currentColor" class="bi bi-pencil-square" >
                                     <path
                                         d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
@@ -220,15 +220,21 @@ export default {
 
 <style>
 .tabla-usuarios{
-
     margin-top: 20px;
     margin-bottom: 20px;
     background-color: #ffffff;
+    box-shadow: #EAD2AC 0px 0px 10px;
     border-radius: 20px;
     padding: 10px;
     display: flex;
     color: #ffffff;
     width: 100rem;
     margin-left: 10rem;
+}
+.editarBoton{
+    border: none;
+    background: none;
+    cursor: pointer;
+    color: #2EBFA5;
 }
 </style>
