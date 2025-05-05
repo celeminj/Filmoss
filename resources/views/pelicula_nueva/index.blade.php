03939528e93e638d9479bb877cefac788198add0
Jordi <jceleming2324@politecnics.barcelona>
2025-05-05 15:07:52 +0200

    vue nuevo para las horas

--- resources/views/pelicula_nueva/index.blade.php
+++ resources/views/pelicula_nueva/index.blade.php
@@ -19,39 +19,42 @@
             <div id="app">
                 <chat :pelicula-id="{{ $pelicula->id }}" :hora="'{{ $hora }}'" :evento-id="{{ $eventoId }}"
                     :usuario-id="{{ auth()->id() }}"></chat>
-            </div>
 
-            <div class="info-peli">
-                <div class="titulo-pelicula">
-                    <h1>{{ $pelicula->titulo }}</h1>
-                    <div class="hora">
-                        <p>Hora seleccionada: <strong>{{ $hora }}</strong></p>
-                    </div>
-                </div>
 
-                <div class="categorias">
-                </div>
-
-                <div class="edad">
-                    <p>+ {{ $pelicula->restriccion_edad }}</p>
-                </div>
-
-                <div class="descripcion-peli">
-                    <p>{{ $pelicula->descripcion }}</p>
-                </div>
-
-                <div class="botones-pelicula">
-                    <div class="ver-ya">
-                        <button>VER YA</button>
-                        <a href="{{ $pelicula->pelicula_src }}">
-                            <div class="play-button"></div>
-                        </a>
-                    </div>
-                    <div class="otros-botones">
-                        <div class="trailer">
-                            <button>Trailer</button>
+                <div class="info-peli">
+                    <div class="titulo-pelicula">
+                        <h1>{{ $pelicula->titulo }}</h1>
+                        <div class="hora">
+                            <p>Hora seleccionada: <strong>{{ $hora }}</strong></p>
                         </div>
                     </div>
+
+                    <div class="categorias">
+                    </div>
+
+                    <div class="edad">
+                        <p>+ {{ $pelicula->restriccion_edad }}</p>
+                    </div>
+
+                    <div class="descripcion-peli">
+                        <p>{{ $pelicula->descripcion }}</p>
+                    </div>
+
+                    <div class="botones-pelicula">
+                        <div class="ver-ya">
+                            <button>VER YA</button>
+                            <a href="{{ $pelicula->pelicula_src }}">
+                                <div class="play-button"></div>
+                            </a>
+                        </div>
+                        <div class="otros-botones">
+                            <div class="trailer">
+                                <button>Trailer</button>
+                            </div>
+                        </div>
+                    </div>
+                    <boton-wait :pelicula-id="{{ $pelicula->id }}" :hora="'{{ $hora }}'"
+                        :pelicula-src="'{{ $pelicula->pelicula_src }}'"></boton-wait>
                 </div>
             </div>
         </div>
@@ -89,17 +92,4 @@
                 @endforeach
             </div>
         </div>
-        <script>
-            document.getElementById('verBtn').addEventListener('click', function() {
-                document.getElementById('contenedorDesaparecer').style.display = 'none';
-                document.getElementById('verBtn').style.display = 'none';
-                document.getElementById('iframePelicula').style.display = 'block';
-
-                document.querySelector('.heart').style.display = 'none';
-                document.querySelector('.play-button').style.display = 'none';
-                document.querySelector('.titulo-pelicula').style.display = 'none';
-                document.querySelector('.edad').style.display = 'none';
-                document.querySelector('.descripcion-peli').style.display = 'none';
-            });
-        </script>
     @endsection
